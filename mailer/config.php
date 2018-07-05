dd3529724332df0744861f9755d29ff053be1e11
Harikrishnan R <harikrishnanr994@gmail.com>
2018-07-05 13:42:49 +0530

    added the mailer library and also connected the contact html page

---
+++ mailer/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$my_email = 'abinvarghese@live.in';
+
+$SMTP = array(
+    'enabled' => true,
+    'host' => 'smtp.ipage.com',
+    'username' => 'vj@vjentertainments.in',
+    'password' => 'Jmm6340#',
+    'port' => 587,
+
+);
